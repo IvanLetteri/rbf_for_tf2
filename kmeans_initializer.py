c56aeca444d78d1e84480577f2c4dc95367d5c55
Ivan Letteri <ivan.informatica09@gmail.com>
2020-05-28 17:08:45 +0200

    Update kmeans_initializer.py

--- kmeans_initializer.py
+++ kmeans_initializer.py
@@ -15,9 +15,9 @@ class InitCentersKMeans(Initializer):
         super().__init__()
 
     def __call__(self, shape, dtype=None):
-        assert shape[1:] == self.X.shape[1:]
-
-        n_centers = shape[0]
+        #assert shape[1:] == self.X.shape[1:]
+        n_centers = 11
+        #n_centers = shape[0]
         km = KMeans(n_clusters=n_centers, max_iter=self.max_iter, verbose=0)
         km.fit(self.X)
         return km.cluster_centers_
